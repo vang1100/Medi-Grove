0b193211fbbac7a3fd765129b45fa408467b1908
Padee Vang <vangpadee11@gmail.com>
2025-05-31 17:04:03 -0500

    added category router

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -4,7 +4,7 @@ const {
 } = require('../modules/authentication-middleware');
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
-const userStrategy = require('../strategies/user.strategy');
+const userStrategy = require('../strategies/user.strategy'); 
 
 const router = express.Router();
 router.get('/', (req, res) => {
