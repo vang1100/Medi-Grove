ef756938850713300487919aaa93a8c736565025
Padee Vang <vangpadee11@gmail.com>
2025-06-16 20:03:09 -0500

    trying to fix journal saga

--- src/redux/sagas/journal.saga.js
+++ src/redux/sagas/journal.saga.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { put, takeEvery, select } from 'redux-saga/effects';
 
-function* fetchJournal() {
+function* fetchJournal(action) {
   
  try {
 
@@ -9,23 +9,15 @@ function* fetchJournal() {
 
     // console.log('journalResponse:', journalResponse);
 
-
-    // yield put({ 
-    //   type: 'SET_JOURNAL', 
-    //   payload: journalResponse.data
-     // Get user ID from Redux auth state
-    const { id: userId } = yield select((state) => state.auth.user);
+    const user_id = action.payload;
     
-    const journalResponse = yield axios.get(`/api/journal/${userId}`, {
-      withCredentials: true // ← Critical for session cookies
-    });
+    const journalResponse = yield axios.get(`/api/journal/${user_id}`);
 
     yield put({ 
       type: 'SET_JOURNAL', 
       payload: journalResponse.data
     });
 
-    
 
   } catch(error){
     console.log('error in fetch journal', error);
