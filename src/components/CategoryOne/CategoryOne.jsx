c816c4833c8a22b0e14705914b505a9147700bfc
Padee Vang <vangpadee11@gmail.com>
2025-06-19 09:43:32 -0500

    edited

--- src/components/CategoryOne/CategoryOne.jsx
+++ src/components/CategoryOne/CategoryOne.jsx
@@ -1,7 +1,23 @@
+import {useSelector, useDispatch} from 'react-redux';
+import {useEffect, useState} from 'react';
 function CategoryOne() {
+
+    const dispatch = useDispatch();
+
+
+    useEffect(() =>
+    {
+        dispatch({type: 'FETCH_CARD' });
+    }, 
+    [])
+
+    const card = useSelector((store) =>store.cardReducer);
+
     return (
         <>
         Random card will populate.
+
+        {JSON.stringify(card)}
         </>
     )
 }
