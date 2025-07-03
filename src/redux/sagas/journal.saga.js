9e18516c0e8fa0e7e62f38e2996a34a266caa996
Padee Vang <vangpadee11@gmail.com>
2025-07-02 19:01:13 -0500

    added comments

--- src/redux/sagas/journal.saga.js
+++ src/redux/sagas/journal.saga.js
@@ -9,10 +9,18 @@ function* fetchJournal(action) {
 
     // console.log('journalResponse:', journalResponse);
 
-     const user = yield select(state => state.user); // Adjust to your 
+     const user = yield select(state => state.user); 
+
+     // yield select () is a function to retrieve data from the application's state (or Redux store). 
+        // it takes in an argument
+     // state here means the entire Redux state object, sometimes called the "state tree"
+     // user here is from the redux
+     // here I am using the function to grab the stae of the user redux. and i assigned it with the varible
+            // called user
     
     const journalResponse = yield axios.get(`/api/journal/${user.id}`);
 
+      // here we are grabbing from the user.id (property)
     yield put({ 
       type: 'SET_JOURNAL', 
       payload: journalResponse.data
