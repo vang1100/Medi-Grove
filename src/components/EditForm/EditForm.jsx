b81fc2d959bb08475cd19a08db5e63129f00a0a0
Padee Vang <vangpadee11@gmail.com>
2025-07-30 18:39:14 -0500

    stuck on edit form

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -1,13 +1,20 @@
-import { useState} from "react";
-import { useDispatch} from 'react-redux';
+import { useState, useEffect} from "react";
+import { useDispatch, useSelector} from 'react-redux';
 
 function EditForm() {
+
+    const journal = useSelector((store)=>store.journalReducer); 
+    
 
     const [title, setTitle] = useState('');
     const [text, setText] = useState('');
 
     const dispatch = useDispatch();
 
+     useEffect(() =>
+        {
+            dispatch({type:'FETCH_JOURNAL'});
+        }, [])
 
 
     const edit = (event) => {
@@ -18,9 +25,46 @@ function EditForm() {
 
     return (
         <>
-         <form onSubmit={edit}>
+
+         {Array.isArray(journal) &&  journal.map((journal => {
+            return (
+                <li key={journal.id}>
+
+                  {/* Title: 
+                  {journal.title} 
+                  <br/>
+                  {journal.text}
+                  <br/>
+                      */}
+
+            <form onSubmit={edit}>
             <input 
-                placeholder="Title"
+                type="text"
+                value={journal.title}
+                onChange={(event) => setTitle(event.target.value)}
+            
+            ></input>
+            <br/>
+            <textarea
+                
+                value={journal.text}
+                onChange={(event) => setText(event.target.value)}
+                
+                
+            />
+
+            
+            <input type="submit" value="Submit"></input>
+        </form>
+
+                </li>
+            )
+        }))}
+
+
+         {/* <form onSubmit={edit}>
+            <input 
+                placeholder={journal.title}
                 type="text"
                 value={title}
                 onChange={(event) => setTitle(event.target.value)}
@@ -29,16 +73,16 @@ function EditForm() {
             <br/>
             <textarea
                 
-                value={text}
+                value={journal.text}
                 onChange={(event) => setText(event.target.value)}
                 
                 
-            />
+            /> */}
 
             
             
-            <input type="submit" value="Submit"></input>
-        </form>
+            {/* <input type="submit" value="Submit"></input>
+        </form> */}
 
         </>
     )
