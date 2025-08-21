c999232d2253af88132595e012e2aaf855ca6c8a
Padee Vang <vangpadee11@gmail.com>
2025-08-21 11:46:08 -0500

    added updateCard in card saga. lets see if works?

--- src/redux/sagas/card.saga.js
+++ src/redux/sagas/card.saga.js
@@ -15,8 +15,52 @@ function * fetchCard() {
     }
 }
 
+function* updateCard(action) {
+
+// yield axios.put(`/api/journal/${action.payload}`)
+  // i assume that this will allow is to change the object from the URL
+
+//   What’s wrong:
+// action.payload is likely an object containing the journal data you want to update (including its id and other fields).
+
+// You need to extract the id from the payload for the URL path and pass the rest of the data as the PUT request body.
+
+ // const { id, ...data } = action.payload;
+        // // First, manually get the id from the payload.
+
+        // Then, create an empty object data.
+
+        // Loop over all keys of action.payload.
+
+        // For each key except "id", copy the property into data.
+  try {
+
+    const { id, ...data } = action.payload;
+
+    console.log('edit journal saga');
+
+    yield axios.put(`/api/cards/${id}`, data);
+
+        //  Because PUT requests typically update a resource with multiple fields, so the server expects:
+
+        // ID in URL to know what to update.
+
+        // Updated data in the body.
+
+    yield put({
+
+      type: 'SET_CARD'
+    });
+
+  }catch(error){
+    console.log('error in updateJournal', error)
+  }
+  
+}
+
 function* cardSaga() {
-yield takeEvery('FETCH_CARD', fetchCard)
+yield takeEvery('FETCH_CARD', fetchCard),
+yield takeEvery('UPDATE_CARD', updateCard)
 }
 
 export default cardSaga;
