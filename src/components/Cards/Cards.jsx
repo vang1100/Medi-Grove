0cc094cd3b1af4cab18234d93194a2c16b770a78
Padee Vang <vangpadee11@gmail.com>
2025-08-26 09:16:57 -0500

    working on favorite page

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+
 function Cards() {
+
+    const cards = useSelector((store) => store.cardReducer)
+    const dispatch = useDispatch();
+
+    useEffect(() =>
+            {
+                dispatch({type: 'FETCH_CARD' });
+            }, 
+                [])
+
+
     return (
         <>
-        This is where the liked cards will appear
+        {/* This is where the liked cards will appear
+        {JSON.stringify(cards)} */}
+
+        
         </>
     )
 }
